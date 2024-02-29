31ad2a96e9ebbc499a2f9a297e8d7985e4c27732
Guillermo Santiago <g.santiago3rd@gmail.com>
2024-02-29 12:36:35 -0700

    Added password confirmation to register page

--- frontend/lottagames/src/pages/RegisterPage.jsx
+++ frontend/lottagames/src/pages/RegisterPage.jsx
@@ -8,11 +8,13 @@ export const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("")
   const [password, setPassword] = useState("");
+  const [confirmedPassword, setconfirmedPassword] = useState("");
   const { setUser } = useContext(userContext);
   const navigate = useNavigate();
 
   const signUp = async (e) => {
     e.preventDefault();
+    if (password === confirmedPassword){
     let response = await api.post("users/register/", {
       email: email,
       name: name,
@@ -21,14 +23,21 @@ export const RegisterPage = () => {
     .catch((err) => {
       alert("Incorrent Credentials")
     })
+    
     let user = response.data.user;
     let token = response.data.token;
+
     // Store the token securely (e.g., in localStorage or HttpOnly cookies)
     localStorage.setItem("token", token);
     api.defaults.headers.common["Authorization"] = `Token ${token}`;
+
     // set the user using with useContext to allow all other pages that need user information
     setUser(user);
     navigate("/profile");
+
+  } else {
+    alert("Passwords do not match.")
+  }
   };
 
   return ( 
@@ -40,20 +49,26 @@ export const RegisterPage = () => {
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        placeholder="email"
+        placeholder="Email"
       />
       <input className="border-[4px] border-x-black border-y-black text-black"
         // username
         type="text"  
         value={name}
         onChange={(e) => setName(e.target.value)}
-        placeholder="User Name"
+        placeholder="Username"
       />
       <input className="border-[4px] border-x-black border-y-black text-black"
         type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
-        placeholder="password"
+        placeholder="Password"
+      />
+      <input className="border-[4px] border-x-black border-y-black text-black"
+        type="password"
+        value={confirmedPassword}
+        onChange={(e) => setconfirmedPassword(e.target.value)}
+        placeholder="Confirm Password"
       />
       <input className="bg-gray-500 hover:bg-gray-600 text-white text-sm font-semibold py-1 px-2 rounded focus:outline-none focus:ring focus:border-blue-300" type="submit" />
     </form>
